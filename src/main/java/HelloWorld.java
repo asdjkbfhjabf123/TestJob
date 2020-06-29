a3b4b888d44cec767cd7ee93626c118f87433eac
asdjkbfhjabf123 <49545318+asdjkbfhjabf123@users.noreply.github.com>
2020-06-29 11:36:50 +0530

    hi

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -11,6 +11,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Welcome Rakesh");
+		get("/hello", (req, res) -> "Welcome Back Rakesh");
 	}
 }
